a3cf01da171fff742dc0a166da2a4460a837f000
Craig Rupp <craigrupp@utexas.edu>
2022-05-30 16:14:28 -0500

    Hardhat local env files for deployment to rinkeby along with deploy script

--- Web3NftTutorial/hardhat.config.js
+++ Web3NftTutorial/hardhat.config.js
@@ -1,21 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const RINKEBY_PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.4",
+
+  // Define test network to deploy to
+  networks : {
+    rinkeby : {
+      url : ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY]
+    },
+    ropsten: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY]
+   }
+  }
 };
